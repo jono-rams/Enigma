7fdb1ca93d11daba7572c72a40737654169608b9
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2021-05-11 12:25:07 -0400

    Added code for user to enter own pairs or use default value

--- Enigma/src/main.cpp
+++ Enigma/src/main.cpp
@@ -6,8 +6,31 @@
 using namespace Enigma;
 int main()
 {
+	s_Pairs* pairs = new s_Pairs[13];
+
+	for (int i = 0; i < 13; i++)
+	{
+		std::cout << "Enter a pair of letters or enter '00' to use default pairs: ";
+		std::cin >> pairs[i].pair1 >> pairs[i].pair2;
+		if (i == 0)
+		{
+			if (pairs[i].pair1 == '0')
+			{
+				delete[] pairs;
+				pairs = nullptr;
+				break;
+			}
+		}
+	}
+
 	Core machine{};
-	machine.GenNewPairModule();
+
+	if (pairs == nullptr)
+		machine.GenNewPairModule();
+	else
+		machine.GenNewPairModule(pairs);
+	delete[] pairs;
+
 	machine.GetPairModule()->DEBUG_PrintPairs();
 	return 0;
 }
